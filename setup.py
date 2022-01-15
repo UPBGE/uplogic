2740e69104f5f87081530c1e6a9ab4858ec93496
Leo <lauersperg@example.com>
2022-01-15 13:03:14 +0100

    revert

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.1'
+version = '1.0'
 shortdesc = "Uplogic utility for UPBGE."
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.md',
@@ -31,7 +31,7 @@ setup(
     author='Leopold Auersperg-Castell',
     author_email='lauersperg@gmx.at',
     url='https://github.com/IzaZed/Uchronian-Logic-UPBGE-Logic-Nodes',
-    download_url='https://github.com/UPBGE/uplogic/archive/refs/tags/v1.1.tar.gz',
+    download_url='https://github.com/UPBGE/uplogic/archive/refs/tags/v1.0.tar.gz',
     license='GPLv2',
     packages=[
         'audio',
