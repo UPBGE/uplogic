f25bc2565a46ad83ae48fb328a1e4cc665fa1708
Leo <lauersperg@example.com>
2022-01-27 20:35:12 +0100

    add material slot support

--- uplogic/nodetrees/materials.py
+++ uplogic/nodetrees/materials.py
@@ -1,9 +1,22 @@
+from bge.types import KX_GameObject as GameObject
+from bpy.types import Material
+from bpy.types import Node
+from bpy.types import NodeSocket
 import bpy
 
 
-def get_material_socket(material, node, socket):
+def get_material_socket(
+    material: str or Material,
+    node: Node,
+    socket: NodeSocket,
+    game_object=None
+):
+    if isinstance(material, str):
+        material = bpy.data.materials[material]
+    if isinstance(material, int):
+        material = game_object.blenderObject.material_slots[0].material
     return (
-        bpy.data.materials[material]
+        material
         .node_tree
         .nodes[node]
         .inputs[socket]
@@ -11,9 +24,13 @@ def get_material_socket(material, node, socket):
     )
 
 
-def set_material_socket(material, node, socket, value):
+def set_material_socket(material, node, socket, value, game_object=None):
+    if isinstance(material, str):
+        material = bpy.data.materials[material]
+    if isinstance(material, int):
+        material = game_object.blenderObject.material_slots[0].material
     (
-        bpy.data.materials[material]
+        material
         .node_tree
         .nodes[node]
         .inputs[socket]
