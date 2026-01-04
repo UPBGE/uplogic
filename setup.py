4e430f6205e395511d0c211bb0745c7db51bc4f6
IzaZed <leo@auersperg.org>
2026-01-04 16:15:18 +0100

    version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(name):
         return f.read()
 
 
-version = '5.1.1'
+version = '5.1.2'
 shortdesc = "Uplogic utility for UPBGE."
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.md',
